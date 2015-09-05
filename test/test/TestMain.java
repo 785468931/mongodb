d1320f5ccd46f86ca18c5358c7df5df3f0a4de85
785468931 <ant_li@qq.com>
2015-09-05 16:48:56 +0800

    添加测试类

---
+++ test/test/TestMain.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.li.mongodb.dao.BindLogMongoDBUtils;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BindLogMongoDBUtils().bindLogListQuery(0, 2);
+	}
+
+}
