3637a663f8419a46ac354d329c5eea12fae40ab0
785468931 <ant_li@qq.com>
2015-09-05 22:21:17 +0800

    修改jdk版本，修改测试类

--- test/test/TestMain.java
+++ test/test/TestMain.java
@@ -2,11 +2,17 @@ package test;
 
 import com.li.mongodb.dao.BindLogMongoDBUtils;
 
+/**
+ * 测试主类
+ * @author ant_li
+ *
+ */
 public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new BindLogMongoDBUtils().bindLogListQuery(0, 2);
+		System.out.println("测试列子");
+//		new BindLogMongoDBUtils().bindLogListQuery(0, 2);
 	}
 
 }
